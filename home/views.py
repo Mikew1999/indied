34774a019856389ab69a6943e2f1b7ce72aadbe3
Mikew1999 <mikewalters1234@gmail.com>
2021-10-19 21:01:01 +0000

    inital commit

--- home/views.py
+++ home/views.py
@@ -4,4 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     ''' Renders home page '''
-    render(request, "index.html")
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = str(request.GET['q'])
+
+    context = {
+        'query': query,
+    }
+
+    return render(request, "home/index.html", context)

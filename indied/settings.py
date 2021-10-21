87e45333429655881cd9618675503a4bc9125149
Mikew1999 <mikewalters1234@gmail.com>
2021-10-21 21:12:19 +0000

    added placeholder left and right icons for settings and messages offcanvas sections. added posts and users apps.

--- indied/settings.py
+++ indied/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--k0rv0^1r32ub0e@c^#8v!s%%ahss!k65%3-wk$hl!--h#9yw2'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'hi')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'posts',
+    'users',
 ]
 
 MIDDLEWARE = [
